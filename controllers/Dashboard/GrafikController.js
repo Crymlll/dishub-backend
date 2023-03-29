def16d581d52c118d9d1eb92859127e1b0977e6b
Aulia Rahman Zulfi <dragoncimol@gmail.com>
2023-03-29 13:58:16 +0700

    grafik

--- controllers/Dashboard/GrafikController.js
+++ controllers/Dashboard/GrafikController.js
@@ -1,4 +1,5 @@
 import Pegawai from "../../models/Pegawai/PegawaiModel.js"
+import { IjazahPegawai } from "../../models/Pegawai/IjazahModel.js"
 import Sequelize from "sequelize"
 
 export const getJenisKelamin = async (req, res) => {
@@ -28,6 +29,27 @@ export const getAgama = async (req, res) => {
 				[Sequelize.fn("COUNT", Sequelize.col("agama")), "jumlah"],
 			],
 			group: ["agama"],
+		})
+		res.status(200).json(response)
+	} catch (error) {
+		console.log(error.message)
+		res.status(404).json({ message: error.message })
+	}
+}
+
+//make get Ijazah sort by tingkat
+
+export const getIjazah = async (req, res) => {
+	try {
+		const response = await IjazahPegawai.findAll({
+			attributes: [
+				"tingkat_ijazah",
+				[
+					Sequelize.fn("COUNT", Sequelize.col("tingkat_ijazah")),
+					"jumlah",
+				],
+			],
+			group: ["tingkat_ijazah"],
 		})
 		res.status(200).json(response)
 	} catch (error) {
--- routes/Dashboard/GrafikRoute.js
+++ routes/Dashboard/GrafikRoute.js
@@ -2,11 +2,13 @@ import express from "express"
 import {
 	getJenisKelamin,
 	getAgama,
+	getIjazah,
 } from "../../controllers/Dashboard/GrafikController.js"
 
 const router = express.Router()
 
 router.get("/count-jenis-kelamin", getJenisKelamin)
 router.get("/count-agama", getAgama)
+router.get("/count-ijazah", getIjazah)
 
 export default router
